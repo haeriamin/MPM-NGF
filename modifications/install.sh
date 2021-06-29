f98b283af807eea14dec51e170b7873e477086a4
Amin <44217824+haeriamin@users.noreply.github.com>
2021-06-29 01:44:10 -0400

    Update install.sh

--- modifications/install.sh
+++ modifications/install.sh
@@ -4,27 +4,27 @@ set -e  # Fail on any error
 set -x  # Display commands being run
 
 cd ~
-sudo rm -R taichi
+sudo rm -fR taichi
 
 # sudo apt-get update -y
 sudo apt install gcc python3-pip
 sudo apt-get install -y python3-dev git build-essential cmake make g++ libx11-dev python3-pyqt5 libtbb2 libtbb-dev
-sudo -H pip3 install numpy scipy pybind11 Flask flask_cors gitpython yapf psutil pyqt5==5.14.0
+sudo pip3 install numpy scipy pybind11 Flask flask_cors gitpython yapf psutil pyqt5==5.14.0
 # sudo apt-get install -y python3-numpy python3-scipy python3-pybind11 python3-flask
 
 wget https://raw.githubusercontent.com/yuanming-hu/taichi/legacy/install.py
-sudo -H python3 install.py
+python3 install.py
 
 source ~/.bashrc
 
 cd ~/taichi/python/taichi/
-sudo wget -O main.py https://github.com/haeriamin/MPM-NGF/raw/master/modifications/main.py
+wget -O main.py https://github.com/haeriamin/MPM-NGF/raw/master/modifications/main.py
 
 cd ~/taichi/include/taichi/dynamics/
-sudo wget -O rigid_body.h https://github.com/haeriamin/MPM-NGF/raw/master/modifications/rigid_body.h
+wget -O rigid_body.h https://github.com/haeriamin/MPM-NGF/raw/master/modifications/rigid_body.h
 
 cd ~/taichi/python/taichi/dynamics/
-sudo wget -O mpm.py https://github.com/haeriamin/MPM-NGF/raw/master/modifications/mpm.py
+wget -O mpm.py https://github.com/haeriamin/MPM-NGF/raw/master/modifications/mpm.py
 
 cd ~
 export TAICHI_NUM_THREADS=32
